0b3bca3a8760068a09e1b98ab90d09473f8e761f
hideonbush3 <ppoii0961@gmail.com>
2023-03-02 17:11:20 +0900

    state를 이용한 환영메세지 출력

--- src/components/EnrollmentForm.js
+++ src/components/EnrollmentForm.js
@@ -1,23 +1,48 @@
 import React from "react";
+import { useState } from "react";
 import "../App.css";
 
 const EnrollmentForm = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [welcomeMessage, setWelcomeMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    setWelcomeMessage(`환영합니다 ${firstName} ${lastName} 님!`);
+    e.preventDefault(); // submit 기능 중지
+  };
   return (
     <div>
-      <form className="enrolForm">
+      <form className="enrolForm" onSubmit={handleSubmit}>
         <h1>대학생 상세 정보 등록 양식</h1>
 
         <div>
           <label>First Name</label>
-          <input type="text" name="fname" />
+          <input
+            type="text"
+            name="fname"
+            /* onBlur 이벤트가 발생하면 그 이벤트를 발생시킨
+            주체의 value로 firstName state의 값을 바꾼다
+            여기서 주체는 onBlur를 포함하고 있는 input태그임 */
+            onBlur={(e) => setFirstName(e.target.value)}
+          />
         </div>
         <div>
           <label>Last Name</label>
-          <input type="text" name="lname" />
+          <input
+            type="text"
+            name="lname"
+            onBlur={(e) => setLastName(e.target.value)}
+          />
         </div>
 
-        <div><button type="submit">등록하기</button></div>
+        <div>
+          <button type="submit">등록하기</button>
+        </div>
       </form>
+      <label id="studentMsg" className="message">
+        {welcomeMessage}
+      </label>
     </div>
   );
 };
