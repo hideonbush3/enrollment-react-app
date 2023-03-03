7c8705d3691be98ca20d0305af44ac894a454aee
hideonbush3 <ppoii0961@naver.com>
2023-03-03 10:59:40 +0900

    props를 이용한 참가가능 인원수 표시 1

--- src/components/EnrollmentForm.js
+++ src/components/EnrollmentForm.js
@@ -14,8 +14,13 @@ const EnrollmentForm = (props) => {
   // 등록하기 버튼 클릭시 '환영합니다 이름 성 님!' 을 폼 아래쪽에 출력함
   const handleSubmit = (e) => {
     setWelcomeMessage(`환영합니다, ${firstName} ${lastName} 님!`);
+    // 참여가능 인원 수 감소
+    // props로 전달받은 함수 setUpdateSeats 를 이용해서
+    // 상위 컴포넌트의 seats 변수를 조작함
+    props.setUpdateSeats(props.currentSeat-1);
     e.preventDefault(); // submit 기능 부모요소에게 전파 중지, 이것을 빼면 submit하고 페이지가 초기화됨
   };
+
   return (
 
     <div>
