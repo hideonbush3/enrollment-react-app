d724277296f7a06cdf1947cc5bce9964dfad5166
hideonbush3 <ppoii0961@naver.com>
2023-03-03 14:50:36 +0900

    UI 변경, 입력항목에 email 추가

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import EnrollmentForm from "./components/EnrollmentForm";
 
 // EnrollmentForm 이라는 폼을 return 하도록
 // 정의된 App 컴포넌트
 const App = () => {
-    const [program, setProgram] = useState('UG');
-    const [ugseats, setUgSeats] = useState(1);    // 참가가능 인원
-    const [pgseats, setPgSeats] = useState(1);    // 참가가능 인원
-    const handleChange = (e) => {
-        setProgram(e.target.value);
-    };
+  const [program, setProgram] = useState("UG");
+  const [ugseats, setUgSeats] = useState(60); // 참가가능 인원
+  const [pgseats, setPgSeats] = useState(40); // 참가가능 인원
+  const handleChange = (e) => {
+    setProgram(e.target.value);
+  };
 
-    // 프로그램별 참가가능 인원수를 변경하는 함수
-    const setUpdateSeats = (modifySeat) => {
-        if(program === "UG") setUgSeats(modifySeat);
-        else setPgSeats(modifySeat);
-    }
+  // 프로그램별 참가가능 인원수를 변경하는 함수
+  const setUpdateSeats = (modifySeat) => {
+    if (program === "UG") setUgSeats(modifySeat);
+    else setPgSeats(modifySeat);
+  };
 
-    return(
-        <div className="App">
-            <div className="programs">
-                <label>학사과정 프로그램 참가가능 인원 수 : {ugseats}</label><br/>
-                <label>석사과정 프로그램 참가가능 인원 수 : {pgseats}</label>
-                <br/><br/>
-                <label>프로그램 종류 : </label>
-                <select className="appDropDowns" onChange={handleChange} value={program}>
-                    <option value="UG">학사과정(대학교)</option>
-                    <option value="PG">석사과정(대학원)</option>
-                </select><br/><br/><br/>
-            </div>
-            <EnrollmentForm chosenProgram={program}
-                            currentSeat={program === 'UG' ? ugseats : pgseats}
-                            setUpdateSeats={setUpdateSeats}/>
-        </div>
-    )
+  return (
+    <div className="App">
+      <div className="programs">
+          <h3 className='title'> 프로그램 참가 등록양식 </h3>
+          <ul className='ulEnrol'>
+              <li onChange={handleChange} className='parentLabels'>
+                  <input type="radio" value="UG" name="programGroup"
+                  defaultChecked />학사과정
+                  <input type="radio" value="PG" name="programGroup"/>석사과정
+              </li>
+              <li> {program} 참가 가능 인원 :
+                  {(program === "UG") ? ugseats : pgseats}</li>
+          </ul>
+      </div>
+      <EnrollmentForm
+        chosenProgram={program}
+        currentSeat={program === "UG" ? ugseats : pgseats}
+        setUpdateSeats={setUpdateSeats}
+      />
+    </div>
+  );
 };
 
 // 컴포넌트나 모듈형태로 작성하는 경우
--- src/components/EnrollmentForm.js
+++ src/components/EnrollmentForm.js
@@ -4,10 +4,11 @@ import "../App.css";
 
 const EnrollmentForm = (props) => {
   // 폼에 입력한 이름과 성을 기억하기 위해서 state형 변수 선언
-  // onBlur 이벤트 발생시 입력한 이름과 성을
-  // firstName, lastName에 저장
+  // onBlur 이벤트 발생시 입력한 이름/성/이메일을
+  // firstName, lastName, email 변수에 저장
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState('');
   // state형 변수에 저장된 이름과 성을 메세지로 출력하기위해 선언
   const [welcomeMessage, setWelcomeMessage] = useState("");
   const [msgStyle, setMsgStyle] = useState("message");
@@ -22,45 +23,55 @@ const EnrollmentForm = (props) => {
     if (props.currentSeat - 1 >= 0) {
       props.setUpdateSeats(props.currentSeat - 1);
       setMsgStyle("message");
-      msg = `환영합니다, ${firstName} ${lastName} 님!`;
+      msg = `환영합니다, ${firstName} ${lastName} 님\n
+       ${email}로 메일을 보냈습니다`;
     }
     setWelcomeMessage(msg);
     e.preventDefault(); // submit 기능 부모요소에게 전파 중지, 이것을 빼면 submit하고 페이지가 초기화됨
   };
 
+  const handleInputChange = (setInput, e) => {
+    setInput(e.target.value);
+  };
   return (
     <div>
+      <div className='enrolContainer'>
       <form className="enrolForm" onSubmit={handleSubmit}>
-        <h1>{props.chosenProgram}대학생 상세 정보 등록 양식</h1>
-
-        <div>
-          <label>First Name</label>
-          <input
-            type="text"
-            name="fname"
-            /* e는 이벤트 객체임 React에서 이벤트 핸들러 함수는
-             항상 이벤트 객체를 인수로 받음 이벤트 객체에는 이벤트에 대한 정보가 담겨있음
-               e.target은 이벤트가 발생한 요소 input을 나타냄
-               e.target.value는 이벤트가 발생한 요소의 값 */
-            onBlur={(e) => setFirstName(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>Last Name</label>
-          <input
-            type="text"
-            name="lname"
-            onBlur={(e) => setLastName(e.target.value)}
-          />
-        </div>
-
-        <div>
-          <button type="submit">등록하기</button>
-        </div>
+        <ul className='ulEnrol'>
+          <li>
+            <label htmlFor='FirstName'></label>
+            <input type="text" id='FirstName'
+                   name='firstName' className='inputFields'
+                   placeholder='FirstName' value={firstName}
+                   onChange={e => handleInputChange(setFirstName, e)} />
+          </li>
+          <li>
+            <label htmlFor='LastName'></label>
+            <input type="text" id='LastName'
+                   name='lastName' className='inputFields'
+                   placeholder='LastName' value={lastName}
+                   onChange={e => handleInputChange(setLastName, e)} />
+          </li>
+          <li>
+            <label htmlFor='Email'></label>
+            <input type="text" id='Email'
+                   name='email' className='inputFields'
+                   placeholder='Email' value={email}
+                   onChange={e => handleInputChange(setEmail, e)} />
+          </li>
+          <li id='center-btn'>
+            <button type='submit' id='btnEnrol'
+            name='enrol' onClick={handleSubmit}>
+              등록하기</button>
+          </li>
+          <li>
+            <label id='studentMsg' className={msgStyle}>
+              {welcomeMessage}
+            </label>
+          </li>
+        </ul>
       </form>
-      <label id="studentMsg" className={msgStyle}>
-        {welcomeMessage}
-      </label>
+      </div>
     </div>
   );
 };
