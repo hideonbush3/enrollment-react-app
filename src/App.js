0df982fce6fc694ddd4d28baf05ef1e6b549e804
hideonbush3 <ppoii0961@naver.com>
2023-03-03 10:00:12 +0900

    state를 이용한 환영메세지 출력

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import React, {useState} from 'react';
+import React from 'react';
 import EnrollmentForm from "./components/EnrollmentForm";
 
+// EnrollmentForm 이라는 폼을 return 하도록
+// 정의된 App 컴포넌트
 const App = () => {
     return(
         <div>
@@ -9,4 +11,6 @@ const App = () => {
     )
 };
 
+// 컴포넌트나 모듈형태로 작성하는 경우
+// 기본적으로 내보내기할 함수명 저장
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+// App 컴포넌트를 가져오기위해 import
 import App from './App.js';
 
+// ReactDOM 라이브러리의 render 함수에 의해
+// index.html의 태그들 중 id가 app인 요소에
+// 해당 컴포넌트를 표시함
+// render(컴포넌트, 표시위치)
 ReactDOM.render(
     <React.StrictMode>
         <h1>Just React</h1>
